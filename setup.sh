5a9adfb8aa82a39b694092862c0a8f37d3221012
sirmackan <marcus@meeloo.se>
2026-04-16 07:45:11 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,13 +10,14 @@ echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
 brew install --cask google-chrome ghostty slack jetbrains-toolbox visual-studio-code dockdoor raycast docker-desktop caddy antigravity codex claude-code
-
 brew install go pure zsh-history-substring-search uv gemini-cli
 
 go install mvdan.cc/gofumpt@latest
 
 uv python install
 uv python update-shell
+
+https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.4/install.sh | bash
 
 git config --global push.autoSetupRemote true
 git config --global alias.bp "pull --rebase --autostash"
@@ -52,5 +53,3 @@ alias cdtmp="cd ~/tmp"
 alias gogetall="go mod tidy && go get -u ./... && go mod vendor"
 alias gofmt="gofumpt -w -l ."
 EOF
-
-https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.4/install.sh | bash
